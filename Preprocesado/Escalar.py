77473fab61bbf62b075d093189f16918e9e0fb26
Yorchz <jorge.hernandez.hdez.02@gmail.com>
2022-02-08 12:00:42 +0000

    2 - modificar conversion

--- Preprocesado/Escalar.py
+++ Preprocesado/Escalar.py
@@ -1,16 +1,12 @@
 class Escalar:
 
     def __init__(self):
-
         self.Xmax = 0
         self.Xmin = 0
         self.max = 0
         self.min = 0
-        self.xPrima = []
-        self.xPrimaInv = []
 
     def ajustar(self, x, miin=-1, maax=1):
-
         self.Xmax = max(x)
         self.Xmin = min(x)
         self.max = maax
@@ -29,16 +25,9 @@ class Escalar:
         return self.max * self.Xmin
 
     def transformar(self, x):
-
-        for i in range(len(x)):
-            self.xPrima.append(round((self.max_minus_min() * x[i]) / self.Xmax_minus_Xmin() +
-                                     ((self.min_mult_Xmax() - self.max_mult_Xmin()) / self.Xmax_minus_Xmin()), 2))
-
-        return self.xPrima
+        return [round((self.max_minus_min() * i) / self.Xmax_minus_Xmin() +
+                      ((self.min_mult_Xmax() - self.max_mult_Xmin()) / self.Xmax_minus_Xmin()), 2) for i in x]
 
     def transformar_inv(self, x):
-
-        for i in range(len(x)):
-            self.xPrimaInv.append(round((x[i] * self.Xmax_minus_Xmin() - self.min_mult_Xmax() +
-                                         self.max_mult_Xmin()) / self.max_minus_min(), 2))
-        return self.xPrimaInv
+        return [round((i * self.Xmax_minus_Xmin() - self.min_mult_Xmax() +
+                       self.max_mult_Xmin()) / self.max_minus_min(), 2) for i in x]

00154b54a1d7a6df5364846bce1d9e69294268e5
Yorchz <jorge.hernandez.hdez.02@gmail.com>
2022-02-16 09:59:47 +0000

    1.4 f

--- Preprocesado/Escalar.py
+++ Preprocesado/Escalar.py
@@ -7,21 +7,16 @@ class Escalar:
         self.min = 0
         self.condicional = 0
         self.maximos_minimos = []
-        self.list = []
-        self.listay = []
 
     def ajustar(self, x, miin=-1, maax=1):
 
-        if not isinstance(x[1], list):
+        if isinstance(x[0], (int, float)):
             self.ajustar_list(x)
             self.ajustar_min_mix(miin, maax)
             self.condicional = 1
-        elif isinstance(x[1], list):
+        else:
             self.ajustar_l_list(x)
             self.ajustar_min_mix(miin, maax)
-            self.condicional = -1
-        else:
-            raise ValueError("No se ha detectado int o list")
 
     def ajustar_list(self, x):
         self.Xmax = max(x)
@@ -51,35 +46,37 @@ class Escalar:
     def transformar(self, x):
         if self.condicional == 1:
             return self.no_matriz_transformar(x)
-        elif self.condicional == -1:
+        else:
             return self.matriz_tranformar(x)
 
     def transformar_inv(self, x):
         if self.condicional == 1:
             return self.no_matriz_transformar_inv(x)
-        elif self.condicional == -1:
+        else:
             return self.matriz_tranformar_inv(x)
 
     def matriz_tranformar(self, x):
+        listy = []
         for i in range(len(x)):
-            self.Xmax = self.maximos_minimos[i][0]
-            self.Xmin = self.maximos_minimos[i][1]
-
-            self.listay.append(self.no_matriz_transformar(x[i]))
-        return self.listay
+            self.sacar_max_min(self.maximos_minimos[i])
+            listy.append(self.no_matriz_transformar(x[i]))
+        return listy
 
     def no_matriz_transformar(self, x):
         return [((self.max_minus_min() * i) + (self.min_mult_Xmax() - self.max_mult_Xmin())) /
                 self.Xmax_minus_Xmin() for i in x]
 
     def matriz_tranformar_inv(self, x):
+        list = []
         for i in range(len(x)):
-            self.Xmax = self.maximos_minimos[i][0]
-            self.Xmin = self.maximos_minimos[i][1]
-
-            self.list.append(self.no_matriz_transformar_inv(x[i]))
-        return self.list
+            self.sacar_max_min(self.maximos_minimos[i])
+            list.append(self.no_matriz_transformar_inv(x[i]))
+        return list
 
     def no_matriz_transformar_inv(self, x):
         return [(i * self.Xmax_minus_Xmin() - self.min_mult_Xmax() +
                  self.max_mult_Xmin()) / self.max_minus_min() for i in x]
+
+    def sacar_max_min(self, maximos_minimos):
+        self.Xmax = maximos_minimos[0]
+        self.Xmin = maximos_minimos[1]
--- Preprocesado/main.py
+++ Preprocesado/main.py
@@ -1,12 +1,12 @@
 from Preprocesado.Escalar import Escalar
 
-prueba = [[5,9,1,3], [2,8,4,6], [4,5,-1,9], [4,5,-1,9]]
-list = [[0.0, 1.0, -1.0, -0.5], [-1.0, 1.0, -0.3333333333333333, 0.3333333333333333], [0.0, 0.2, -1.0, 1.0], [0.0, 0.2, -1.0, 1.0]]
+prueba = [[6.6, 4.5, 7], [7 ,9, 4]]
+lista = [[0.6799999999999997, -1.0, 1.0], [0.2, 1.0, -1.0]]
 
 escalar = Escalar()
 
 escalar.ajustar(prueba)
 print(escalar.transformar(prueba))
-print(escalar.transformar_inv(list))
+print(escalar.transformar_inv(escalar.transformar(prueba)))
 
 
